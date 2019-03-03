931035c7aee73006bb720b5c0df4a113f4275982
Marlster <marley7898@gmail.com>
2019-03-03 14:11:56 +0000

    et

--- Match.js
+++ Match.js
@@ -117,7 +117,7 @@ export default class Match extends Component {
   render() {
     if (this.state.matching) {
       return (
-        <ImageBackground source={require('./assets/ganda.jpg')} style={styles.backgroundStyle}>
+        <ImageBackground source={require('./assets/ganda.png')} style={styles.backgroundStyle}>
           <View style={styles.container}>
             <Text style={styles.textContainer}>Matching. Please Wait.</Text>
           </View>
