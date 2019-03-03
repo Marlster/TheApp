ea45249ca3a8fd31dc2209f4b6c00ee2b442c490
Marlster <marley7898@gmail.com>
2019-03-03 04:00:21 +0000

    and more

--- Tracker.js
+++ Tracker.js
@@ -20,7 +20,7 @@ export default class Tracker extends Component {
         errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
       });
     } else {
-      this._getLocationAsync();
+      this.getDistance();
     }
   }
 
